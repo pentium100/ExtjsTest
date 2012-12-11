1bf11a635e601bdc76696c703c9f65c93d233ead
lenovo <lenovo@lenovo-PC>
2012-12-11 14:48:12 +0800

    add batchno to inspection list.

--- ExtjsTest/src/main/webapp/app/view/inspection/List.js
+++ ExtjsTest/src/main/webapp/app/view/inspection/List.js
@@ -2,22 +2,21 @@ Ext.define('AM.view.inspection.List', {
 			extend : 'Ext.grid.Panel',
 			alias : 'widget.inspectionList',
 
-
 			store : 'inspection.Inspections',
 
 			requires : ['Ext.toolbar.TextItem', 'Ext.ux.grid.FiltersFeature'],
 
 			iconCls : 'icon-grid',
 			frame : true,
-			stateful: true,
-   			stateId: 'inspectionList',			
+			stateful : true,
+			stateId : 'inspectionList',
 
 			initComponent : function() {
 				this.columns = [{
 							header : '合同号',
 							dataIndex : 'contracts',
 							filterable : true,
-							width: 120
+							width : 120
 						}, {
 							header : '检验日期',
 							dataIndex : 'inspectionDate',
@@ -40,6 +39,29 @@ Ext.define('AM.view.inspection.List', {
 							header : '检验后规格',
 							dataIndex : 'model_tested',
 							filterable : true
+						}, {
+							header : '批次号',
+							dataIndex : 'batchNos',
+							filterable : true,
+							flex : 4,
+							renderer : function(value, metaData, rec) {
+
+								var text = ',';
+
+								var items = rec.items();
+								for (var i = 0; i < items.getCount(); i++) {
+									var item = items.getAt(i);
+									var model = item.get('batchNo');
+									if (text.indexOf(',' + model + ',') == -1) {
+										text = text + model + ','
+									}
+								}
+
+								text = text.substr(1, 1000);
+								return text;
+
+							}
+
 						}, {
 							header : '备注',
 							dataIndex : 'remark',
